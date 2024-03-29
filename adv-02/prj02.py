67526a1d4e059e66390da8b1c2b13d8f0c1920af
Wilson487 <wilsonchu2012@gmail.com>
2024-03-29 19:32:33 +0800

    hw4

---
+++ adv-02/prj02.py
@@ -0,0 +1,82 @@
+###################匯入模組###################
+import pygame
+import sys
+import math
+import os
+import sys
+import time 
+##################定義函式##################
+def check_click(pos,x_mix,y_mix,x_max,y_max):
+    x_match=x_mix<pos[0]<x_max
+    y_match=y_mix<pos[1]<y_max
+    if x_match and y_match:
+        
+        pygame.mixer.music.load(mp3_path)
+        pygame.mixer.music.play()
+        pygame.mixer.music.fadeout(600000)
+        time.sleep(0.1)
+        return True
+    else:
+        return False
+###################初始化####################
+os.chdir(sys.path[0])
+width = 500
+height= 500
+pygame.init()
+bg_img="snow.jpg"
+bg=pygame.image.load(bg_img)
+BLACK=(0,0,0)
+bg_x=bg.get_width()
+bg_y=bg.get_height()
+
+###################建立視窗及物件###################
+#設定窗大小
+screen=pygame.display.set_mode((width,height))
+pygame.display.set_caption("Minecraft")
+screen=pygame.display.set_mode((bg_x,bg_y))
+pygame.display.set_caption("snow")
+###################撥放音樂###################
+mp3_path="snow-dream.mp3"
+pygame.mixer.music.load(mp3_path)
+pygame.mixer.music.play()
+pygame.mixer.music.fadeout(600000)
+###################設定文字###################
+typeface=pygame.font.get_default_font()
+font=pygame.font.Font(typeface,24)
+title=font.render("Start",True,(0,0,0))
+tw=title.get_width()
+th=title.get_height()
+###################設定雪花基本參數###########
+
+
+###################新增fps###################
+clock=pygame.time.Clock()
+###################建立畫布###################
+# bg=pygame.Surface((width,height))
+# bg.fill((0,0,0))
+###################循環偵測###################
+paint=False
+while True:
+    clock.tick(20)
+    mouse_pos=pygame.mouse.get_pos()
+
+    # print(mouse_pos)
+    for event in pygame.event.get():
+        if event.type ==pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if check_click(mouse_pos,0,0,tw,th):
+                paint=not paint
+    if paint:
+        title=font.render("start",True,BLACK)
+        pygame.mixer.music.pause()
+
+    else:
+        title=font.render("stop",True,BLACK)
+        pygame.mixer.music.unpause()
+
+
+
+    screen.blit(bg ,(0, 0))
+    screen.blit(title,(0,0))
+    pygame.display.update()
--- prje-01-windows-hw .py
+++ prje-01-windows-hw .py
@@ -2,24 +2,51 @@
 import pygame
 import sys
 import math
-###################初始化###################
+##################定義函式##################
+def check_click(pos,x_mix,y_mix,x_max,y_max):
+    x_match=x_mix<pos[0]<x_max
+    y_match=y_mix<pos[0]<y_max
+    if x_match and y_match:
+        return True
+    else:
+        return False
+###################初始化####################
 pygame.init()
 width = 500
 height= 500
 ###################建立視窗及物件###################
 #設定窗大小
-s=pygame.display.set_mode((width,height))
+screen=pygame.display.set_mode((width,height))
 pygame.display.set_caption("Minecraft")
+###################設定文字###################
+typeface=pygame.font.get_default_font()
+font=pygame.font.Font(typeface,24)
+title=font.render("Start",True,(0,0,0))
+tw=title.get_width()
+th=title.get_height()
 ###################建立畫布###################
 bg=pygame.Surface((width,height))
-bg.fill((255,255,255))
+bg.fill((0,0,0))
 ###################循環偵測###################
+paint=False
 while True:
+    screen.blit(bg,(0,0))
+    mouse_pos=pygame.mouse.get_pos()
+    # print(mouse_pos)
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
             sys.exit()
     if event.type == pygame.MOUSEBUTTONDOWN:
-        s.blit(bg,(0,0))
+        if check_click(mouse_pos,0,0,tw,th):
+            paint=not paint
+    if paint:
+        pygame.draw.circle(bg, (255,0,250),(200,100),30,0)
+        pygame.draw.circle(bg, (255,0,250),(400,100),30,0)
+        pygame.draw.rect(bg, (100,50,125),[270,130,60,40],5)
+        pygame.draw.ellipse(bg, (255,0,0),[130,160,60,35],5)
+        pygame.draw.ellipse(bg, (255,0,0),[400,160,60,35],5)
+        pygame.draw.line(bg, (255,0,250),(200,220),(320,220),3)
+
         pygame.display.update()
         ###################繪製圖形###################
         pygame.draw.circle(bg, (255,0,250),(70,100),30,0)
