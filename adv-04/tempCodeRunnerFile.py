a36aa2bd20f5c7c0ab22a16a98a2331ebf9a5bdd
Wilson487 <wilsonchu2012@gmail.com>
2024-03-31 17:31:56 +0800

    hw5

--- adv-04/tempCodeRunnerFile.py
+++ adv-04/tempCodeRunnerFile.py
@@ -1,2 +1,5 @@
-
-    gophers_update()
+    global tick,pos,score,times
+        if tick>max_tick:
+            new_pos=random.randint(0,5)
+            pos=pos6[new_pos]
+            tick=0
--- adv-05/prj05.py
+++ adv-05/prj05.py
@@ -1,11 +1,8 @@
 ######################匯入模組######################
 import pygame
 import sys
-import math
 import os
-import sys
-import time
-import random 
+import random
 ####################定義函式######################
 def gophers_update():
     global tick,pos,score,times
@@ -15,37 +12,33 @@ def gophers_update():
         tick=0
     else:
         tick+=1
+        times+=1
     screen.blit(gopher,(pos[0]-gopher.get_width()/2,pos[1]-gopher.get_height()/2))
-
-
-def gopher_update():
-    global tick,pos,score
-    if tick > max_tick:
-        now_pos=random.randint(0,5)
-        pos=pos6[now_pos]
-        tick=0
-
-        
-    else:
-        tick+=1
-
-    
-    
-
-
-
 def score_update():
     score_sur=score_font.render(str(score),False,red)
     screen.blit(score_sur,(10,10))
-def check_click(pos,x_mix,y_mix,x_max,y_max):
-    x_match=x_mix<pos[0]<x_max
-    y_match=y_mix<pos[1]<y_max
+def check_click(pos,x_min,y_min,x_max,y_max):
+    x_match=x_min<pos[0]<x_max
+    y_match=y_min<pos[1]<y_max
     if x_match and y_match:
         return True
     else:
         return False
-    screen.blit(gophers,(pos[0]-gophers.get_width()/2,pos[1].gophers.get_height()/2))
-    screen.blit(gopher,[pos6])
+def times_update():
+    times_sur=times_font.render(str(times),True,red)
+    screen.blit(times_sur,(bg_x-times_sur.get_width()-10,10))
+def game_over():
+    screen.fill(black)
+    end_sur=score_font.render(f"Your score is :{score}",False,red)
+    screen.blit(end_sur,(bg_x/2-end_sur.get_width()/2,bg_y/2-end_sur.get_height()/2))
+def mouse_update():
+    global hammer,hammer_tick
+    if hammer==ham1:
+        hammer=ham2
+        hammer_tick=0
+    else:
+        hammer_tick += 1
+    screen.blit(hammer,(mouse_pos[0]-15,mouse_pos[1]-15))
 ####################初始化######################
 os.chdir(sys.path[0])
 pygame.init()
@@ -60,45 +53,52 @@ bg_img='Gophers_BG_800x600.png'
 bg=pygame.image.load(bg_img)
 bg_x=bg.get_width()
 bg_y=bg.get_height()
-
 ######################建立視窗######################
 bg_x = 800
 bg_y = 600
 screen = pygame.display.set_mode([bg_x,bg_y])
 pygame.display.set_caption("打地鼠")
 ######################背景物件######################
+# 將背景填滿黑色
+######################次數物件######################
+times=0
+times_max=5
+typeface=pygame.font.get_default_font()
+times_font=pygame.font.Font(typeface,24)
 ######################地鼠物件######################
-pos6=[[195,305],[400,305],[610,305],[195,450],[400,450],[610,450]]
-
-# pos6=[[200,200],[300,200],[400,200],[200,300],[300,300],[400,300]]
-pos=pos6[0]
-sb="Gophers150.png"
-gophers=pygame.image.load(sb)
-gophers=pygame.image.load('Gophers_BG_800x600.png')
+pos6 = [[195,305],[400,305],[610,305],[195,450],[400,450],[610,450]]
+# pos6 = [[200,200],[300,200],[400,200],[200,300],[300,300],[400,300]]
+pos = pos6[0] # 外圍記錄圓的位子
 gopher = pygame.image.load("Gophers150.png") # 地鼠圖片
-
-        
-    
+gophers=pygame.image.load('Gophers_BG_800x600.png')
 ######################分數物件######################
 score=0
 typeface=pygame.font.get_default_font()
 score_font=pygame.font.Font(typeface,24)
 ######################滑鼠物件######################
-
+pygame.mouse.set_visible(False)
+# ham1=pygame.image.load("Hammer1.png")
+# ham2=pygame.image.load("Hammer2.png")
+# hammer=ham2
+# hammer_tick=0
+# hammer_max_tick=5
 ######################循環偵測######################
 while True:
     clock.tick(30)
     mouse_pos=pygame.mouse.get_pos()
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type==pygame.QUIT:
             sys.exit()
         if event.type==pygame.MOUSEBUTTONDOWN:
             if check_click(mouse_pos,pos[0]-50,pos[1]-50,pos[0]+50,pos[1]+50):
                 tick=max_tick+1
-                score +=1
+                score+=1
+    if times>=times_max:
+        game_over()
+    else:
         screen.blit(bg,(0,0))
         gophers_update()
-        gophers_update()
+        pygame.draw.circle(screen,red,mouse_pos,10)
         score_update()
+        times_update()
     pygame.display.update()
-Gophers_BG_800x600.png
